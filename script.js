0dd6851c42166dcdb8d3527d67065978505dfa43
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-12 21:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function isSameType(value1, value2) {
-  if(isNaN(value1 ||value2)) {
+   if(isNaN(value1) && isNaN(value2)) {
+    return true;
+  }
+  else if(isNaN(value1) || isNaN(value2)) {
     return false;
   }
   else if(typeof value1 == typeof value2) {
